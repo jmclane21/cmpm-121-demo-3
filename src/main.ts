fb60c13a81e6353ee38fca2c9a966e4686b56d08
Jackson McLane <jmclane@ucsc.edu>
2024-11-04 11:55:33 -0800

    testing dev environment

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 // todo
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const testButton = document.createElement("button");
+testButton.innerHTML = "Test Button";
+testButton.onclick = () => {
+  alert("Test Button Clicked!");
+};
+app.append(testButton);
